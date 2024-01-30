61ce0e2b8d04ca3c8e6040e64205e3d9d5aca668
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-01-30 17:55:13 -0300

    feat: add home-manager
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,11 +1,10 @@
 {
-  description = "NixOS Flake";
+  description = "NixOS Flake configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    # home-manager, used for managing user configuration
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
