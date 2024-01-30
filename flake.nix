5dd5449162cab3807c6f8b531aa30307f5324062
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-01-30 18:33:52 -0300

    fix: fix home-manager
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- flake.nix
+++ flake.nix
@@ -17,6 +17,18 @@
 
         modules = [
           ./configuration.nix
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # TODO replace ryan with your own username
+            home-manager.users.fabiosouzadev = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
         ];
       };
     };
