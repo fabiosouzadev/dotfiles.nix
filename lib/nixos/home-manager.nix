2d471bd4055e8af235813e14e0bdb31f154649f1
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-05 19:41:08 -0300

    chore: change some things
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- lib/nixos/home-manager.nix
+++ lib/nixos/home-manager.nix
@@ -96,19 +96,19 @@
 #  };
 
   # starship - an customizable prompt for any shell
-  programs.starship = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    enableNushellIntegration = true;
-    # custom settings
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
-    };
-  };
+  # programs.starship = {
+  #   enable = true;
+  #   enableBashIntegration = true;
+  #   enableZshIntegration = true;
+  #   enableNushellIntegration = true;
+  #   # custom settings
+  #   settings = {
+  #     add_newline = false;
+  #     aws.disabled = true;
+  #     gcloud.disabled = true;
+  #     line_break.disabled = true;
+  #   };
+  # };
 
   # alacritty - a cross-platform, GPU-accelerated terminal emulator
   programs.alacritty = {
@@ -118,7 +118,6 @@
       env.TERM = "xterm-256color";
       font = {
         size = 12;
-        draw_bold_text_with_bright_colors = true;
       };
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
