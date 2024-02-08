7b364099ef0daec4f7626e31ee8f64f8f38bb08b
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-08 14:16:50 -0300

    chore: try new picom configs
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/i3.nix
+++ modules/i3.nix
@@ -72,11 +72,8 @@
 
   services.picom = {
     enable = true;
-    fade = true;
     activeOpacity = 1.0;
     inactiveOpacity = 0.9;
-    fadeDelta = 3; # default 10
-
     # this was the thing that made the tearing go away!
     backend = "glx";
   };
