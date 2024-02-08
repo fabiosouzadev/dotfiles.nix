7fda08e0d4bb496aaccdf1ca02815f6ac64cc33b
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-08 14:55:16 -0300

    chore: new rules picom - again
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/i3.nix
+++ modules/i3.nix
@@ -72,14 +72,6 @@
 
   services.picom = {
     enable = true;
-    package = pkgs.picom.overrideAttrs (_: {
-      src = pkgs.fetchFromGitHub {
-        repo = "picom";
-        owner = "ibhagwan";
-        rev = "44b4970f70d6b23759a61a2b94d9bfb4351b41b1";
-        sha256 = "0iff4bwpc00xbjad0m000midslgx12aihs33mdvfckr75r114ylh";
-      };
-    });
   };
   
 }
