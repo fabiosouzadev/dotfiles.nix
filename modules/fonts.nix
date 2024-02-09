8dc9518926e136c1ecc3223a22b493309c77a980
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-09 12:36:09 -0300

    fix: split fonts configs
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,9 +1,17 @@
 { pkgs, ... }: 
 {
-  fonts.fontconfig.enable = true;
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { 
-      fonts = [ 
+    packages = with pkgs; [
+      # icon fonts
+      material-design-icons
+
+      # normal fonts
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+
+      # nerdfonts
+      (nerdfonts.override {
+        fonts = [ 
           "CascadiaCode"
           "FiraCode"
           "FiraMono"
@@ -25,7 +33,19 @@
           "RobotoMono"
           "Terminus"
           "DejaVuSansMono"
-      ]; 
-    })
-  ];
-}
+        ];})
+    ];
+
+    # use fonts specified by user rather than default ones
+    enableDefaultPackages = false;
+
+    # user defined fonts
+    # the reason there's Noto Color Emoji everywhere is to override DejaVu's
+    # B&W emojis that would sometimes show instead of some Color emojis
+    # fontconfig.defaultFonts = {
+    #   serif = ["Noto Serif" "Noto Color Emoji"];
+    #   sansSerif = ["Noto Sans" "Noto Color Emoji"];
+    #   monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+    #   emoji = ["Noto Color Emoji"];
+    # };
+  };
