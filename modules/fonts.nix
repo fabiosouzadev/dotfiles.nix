7fbad2b5bed53675d4606f94e6659627a2755731
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-09 12:16:39 -0300

    feat: split fonts configx
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

---
+++ modules/fonts.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }: 
+{
+  fonts.fontconfig.enable = true;
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { 
+      fonts = [ 
+          "CascadiaCode"
+          "FiraCode"
+          "FiraMono"
+          "Hack"
+          "JetBrainsMono"
+          "VictorMono"
+          "IBMPlexMono"
+          "SourceCodePro"
+          "SpaceMono"
+          "NerdFontsSymbolsOnly"
+          "Ubuntu"
+          "UbuntuMono"
+          "Inconsolata"
+          "Iosevka"
+          "IosevkaTerm"
+          "FantasqueSansMono"
+          "DroidSansMono"
+          "Noto"
+          "RobotoMono"
+          "Terminus"
+          "DejaVuSansMono"
+      ]; })
+  ];
+}
