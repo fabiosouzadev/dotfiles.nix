1f285ce6a2d24a02edf290b70da8d303b25d72c8
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-09 11:01:56 -0300

    feat: add polybar for home-manager
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- lib/nixos/home-manager.nix
+++ lib/nixos/home-manager.nix
@@ -13,15 +13,40 @@
     authy
     bitwarden
   ];
+
   services.polybar = {
-      enable = true;
-      script = "exec polybar main &";
-      package = pkgs.polybar.override {
-        i3GapsSupport = true;
-        alsaSupport = true;
-        iwSupport = true;
-        githubSupport = true;
+    enable = true;
+    package = pkgs.polybar.override {
+      i3Support = true;
+      alsaSupport = true;
+      iwSupport = true;
+      githubSupport = true;
+    };
+    config = {
+      "bar/top" = {
+        monitor = "eDP1";
+        width = "100%";
+        height = "3%";
+        radius = 0;
+        # Just sticking them together in the center for now
+        modules-center = "date i3";
       };
+      "module/date" = {
+        type = "internal/date";
+        internal = 5;
+        date = "%Y-%m-%d";
+        time = "%H:%M";
+        label = "%date% %time%";
+      };
+      "module/i3" = {
+        type = "internal/i3";
+        scroll-up = "i3wm-wsnext";
+        scroll-down = "i3wm-wsprev";
+      };
+    };
+    script = ''
+    polybar top &
+    '';
   };
 
   home.file."/.config/wallpapers" = {
