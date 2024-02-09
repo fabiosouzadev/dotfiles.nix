f7467a4513da2b285553f3507579b4c2426da170
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-09 11:07:33 -0300

    feat: polybar installed
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- lib/nixos/home-manager.nix
+++ lib/nixos/home-manager.nix
@@ -22,28 +22,6 @@
       iwSupport = true;
       githubSupport = true;
     };
-    config = {
-      "bar/top" = {
-        monitor = "eDP1";
-        width = "100%";
-        height = "3%";
-        radius = 0;
-        # Just sticking them together in the center for now
-        modules-center = "date i3";
-      };
-      "module/date" = {
-        type = "internal/date";
-        internal = 5;
-        date = "%Y-%m-%d";
-        time = "%H:%M";
-        label = "%date% %time%";
-      };
-      "module/i3" = {
-        type = "internal/i3";
-        scroll-up = "i3wm-wsnext";
-        scroll-down = "i3wm-wsprev";
-      };
-    };
     script = ''
     polybar top &
     '';
