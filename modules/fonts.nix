d9a48682484193ba0b03adabfdbbe0162deccd96
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-14 12:07:10 -0300

    feat: add fonts configs

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -5,9 +5,9 @@
     material-design-icons
 
     # normal fonts
-    #noto-fonts
-    #noto-fonts-cjk
-    #noto-fonts-emoji
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
 
     # nerdfonts
     (nerdfonts.override {fonts = [
@@ -41,10 +41,10 @@
   # user defined fonts
   # the reason there's Noto Color Emoji everywhere is to override DejaVu's
   # B&W emojis that would sometimes show instead of some Color emojis
-  # fontconfig.defaultFonts = {
-  #   serif = ["Noto Serif" "Noto Color Emoji"];
-  #   sansSerif = ["Noto Sans" "Noto Color Emoji"];
-  #   monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
-  #   emoji = ["Noto Color Emoji"];
-  # };
+    fontconfig.defaultFonts = {
+	serif = ["Noto Serif" "Noto Color Emoji"];
+  	sansSerif = ["Noto Sans" "Noto Color Emoji"];
+  	monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+  	emoji = ["Noto Color Emoji"];
+    };
 }
