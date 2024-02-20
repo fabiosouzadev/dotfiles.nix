ecf4a7ebbfec19fab7d4a26c4c1130857c1c151f
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-20 17:40:37 -0300

    refactor: get user on cachix

--- modules/services/nix.nix
+++ modules/services/nix.nix
@@ -11,7 +11,7 @@
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
-      trusted-users = [ "fabiosouzadev" ];
+      trusted-users = [ defaultUser ];
       warn-dirty = false;
     };
     # do garbage collection weekly to keep disk usage low
