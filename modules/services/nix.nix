c4d1fe55248a46308f3b41167e3c65adcea2f2bc
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-20 15:45:35 -0300

    refactor: fix nix configs
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/services/nix.nix
+++ modules/services/nix.nix
@@ -1,25 +1,25 @@
 { nixpkgs, defaultUser }: {
-  nix = {
-    settings = {
-      auto-optimise-store = true;
-      builders-use-substitutes = true;
-      experimental-features = [ "nix-command" "flakes" ];
-      substituters = [
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-      trusted-users = [ defaultUser ];
-      warn-dirty = false;
-    };
-    # do garbage collection weekly to keep disk usage low
-    gc = {
-      automatic = nixpkgs.lib.mkDefault true;
-      dates = nixpkgs.lib.mkDefault "weekly";
-      options = nixpkgs.lib.mkDefault "--delete-older-than 3d";
-    };
-  };
+  # nix = {
+  #   settings = {
+  #     auto-optimise-store = true;
+  #     builders-use-substitutes = true;
+  #     experimental-features = [ "nix-command" "flakes" ];
+  #     substituters = [
+  #       "https://nix-community.cachix.org"
+  #     ];
+  #     trusted-public-keys = [
+  #       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  #     ];
+  #     trusted-users = [ defaultUser ];
+  #     warn-dirty = false;
+  #   };
+  #   # do garbage collection weekly to keep disk usage low
+  #   gc = {
+  #     automatic = nixpkgs.lib.mkDefault true;
+  #     dates = nixpkgs.lib.mkDefault "weekly";
+  #     options = nixpkgs.lib.mkDefault "--delete-older-than 3d";
+  #   };
+  # };
   nixpkgs.config.allowUnfree = true;
   services.nix-daemon.enable = true;
 }
