bece727569d46c1ede30910a7cad0340357965dd
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-20 12:08:10 -0300

    refactor: adjust nix pkgs
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   description = "NixOS configuration of Fabio Souza (fabiosouzadev)";
   inputs = {
-	  nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
   };
   
