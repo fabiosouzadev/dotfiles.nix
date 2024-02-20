4a1206d06c545f870bad24bc92472f42ed81d238
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-20 15:24:46 -0300

    refactor: fix user home imports
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/desktops/user.nix
+++ modules/desktops/user.nix
@@ -2,7 +2,8 @@
  
 {
   # TODO please change the username & home direcotry to your own
-  home.username = defaultUser;
-  home.homeDirectory = "/home/${defaultUser}";
-
+  home-manager.users.${defaultUser} = {
+    home.username = defaultUser;
+    home.homeDirectory = "/home/${defaultUser}";
+  };
 }
