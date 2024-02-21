c9fdba4bcad720b4720414ffc9fa434e7afee756
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-21 18:50:49 -0300

    fix: fix signing git commits
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/shells/git.nix
+++ modules/shells/git.nix
@@ -7,6 +7,10 @@
       enable = true;
       userName = defaultGit.userName;
       userEmail = defaultGit.userEmail;
+      signing = {
+      	key = "5884A73AFE6C0B1E5168B2DA9D79F9FD64781516";
+      	signByDefault = true;
+      };
     };
    };
 }
