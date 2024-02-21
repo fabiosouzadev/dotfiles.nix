ba31df5fbe1693b54dc5a108a18f082200e9f494
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-21 18:25:25 -0300

    fix: fix gnupg
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/shells/gpg.nix
+++ modules/shells/gpg.nix
@@ -1,10 +1,12 @@
 {  config, pkgs, defaultUser, defaultGit, ... }:
 {
-  #home-manager.users.${defaultUser} = {
-    programs.gnupg.agent = {
+  home-manager.users.${defaultUser} = {
+    programs.gpg.enable = true;
+    services.gpg-agent = {
+      defaultCacheTtl = 31536000; # cache keys forever don't get asked for password
       enable = true;
-      enableSSHSupport = true;
+      maxCacheTtl = 31536000;
       pinentryFlavor = "tty";
     };
-  #};
+  };
 }
