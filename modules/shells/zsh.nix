19a94de23059e6f209e2185d3c1cb6f970d16aae
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-21 20:12:37 -0300

    feat: add git configs
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/shells/zsh.nix
+++ modules/shells/zsh.nix
@@ -7,8 +7,8 @@
     enableCompletion = true;
     syntaxHighlighting.enable = true;
     histSize = 100000;
-    sessionVariables = {
-     GPG_TTY= "$(tty)";
-    };
+    #initExtra = ''
+    # export GPG_TTY= "$(tty)"
+    #'';
   };
 }
