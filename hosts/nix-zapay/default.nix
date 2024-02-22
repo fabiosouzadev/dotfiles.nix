afda2c33cdab50cc89a5ca06ac16292ecf4ef3e4
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-22 10:41:59 -0300

    fix: rollback kernel
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- hosts/nix-zapay/default.nix
+++ hosts/nix-zapay/default.nix
@@ -20,8 +20,8 @@
   };
 
   boot.loader.grub.device = "nodev";
-  #boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.extraModulePackages = [ config.boot.kernelPackages.rtl8821ce ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.extraModulePackages = [ config.boot.kernelPackages.rtl8821ce ];
   
   # Enable networking
   networking.networkmanager.enable = true;
