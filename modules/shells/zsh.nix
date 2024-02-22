2a2674e5c109b9d47eab2b97ace0777d13e91172
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-02-22 11:23:53 -0300

    fix: fix tty
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/shells/zsh.nix
+++ modules/shells/zsh.nix
@@ -1,14 +1,25 @@
-{ config,pkgs, defaultUser, ... }: { 
+
+{  config, pkgs, defaultUser, defaultGit, ... }:
+
+{
+  # basic configuration of git, please change to your own
   environment.shells = [ pkgs.zsh ];
   users.users.${defaultUser}.shell = pkgs.zsh;
-  programs.zsh = {
-    enable = true;
-    autosuggestions.enable = true;
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
-    histSize = 100000;
-    #initExtra = ''
-    # export GPG_TTY= "$(tty)"
-    #'';
-  };
+  programs.zsh.enable = true;
+  home-manager.users.${defaultUser} = {
+    programs.zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+      history = {
+        save = 100000;
+        size = 100000;
+        share = true;
+      };
+      #initExtra = ''
+      # export GPG_TTY= "$(tty)"
+      #'';
+   };
+ };  
 }
