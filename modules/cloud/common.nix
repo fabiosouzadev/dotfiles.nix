48c443c8b19c600d0aee47e0d6daa652c3fd24cc
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-03-06 08:42:43 -0300

    feat: add cloud tools, add kubeswitch configs
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/cloud/common.nix
+++ modules/cloud/common.nix
@@ -11,6 +11,40 @@
 	k9s
 	kubeswitch
     ];
+
+    home.file.".kube/switch-config.yaml".text = ''
+    kind: SwitchConfig
+    version: v1alpha1
+    kubeconfigStores:
+    - kind: filesystem
+      id: config
+      paths:
+       - "~/.kube/"
+   - kind: filesystem
+     id: zapay
+     kubeconfigName: "zapay-*"
+     paths:
+    - "~/.kube/zapay/"
+   # - kind: gke
+   #   refreshIndexAfter: 3h
+   #   config:
+   #     # optionally set the account. Otherwise, the currently active gcloud account will be used.
+   #     gcpAccount: fabio.souza@ciahering.com.br
+   #     authentication:
+   #       authenticationType: gcloud
+   #   cache:
+   #     kind: filesystem
+   #     config:
+   #       path: ~/.cache/kube/
+   #     # optionally limit to certain projects in account
+   #     # projectIDs:
+   #     #   - hering-integracoes-b
+    '';
+   # home.file.".kube/zapay" = {
+   # source = ../../zapay;
+   # recursive = true;
+   # };
+
   };
 
 }
