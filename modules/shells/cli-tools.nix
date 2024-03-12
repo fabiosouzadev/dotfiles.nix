0f5edf7c674cd29c36e2d4963d93d95836725ef4
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-03-12 14:28:57 -0300

    chore(cli-tools): remove grep alias
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/shells/cli-tools.nix
+++ modules/shells/cli-tools.nix
@@ -62,7 +62,7 @@
        '';
       shellAliases = {
 	"cat" = "${pkgs.bat}/bin/bat";
-	"grep" = "${pkgs.ripgrep}/bin/rg";
+	# "grep" = "${pkgs.ripgrep}/bin/rg";
 	"l" = "${pkgs.eza}/bin/eza -lF --time-style=long-iso --icons";
         "la" = "${pkgs.eza}/bin/eza -lah --tree";
         "ls" = "${pkgs.eza}/bin/eza -h --git --icons --color=auto --group-directories-first -s extension";
