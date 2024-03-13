cc776ad2426125f6a80eb7ed0d0f5d447d8e81cb
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-03-13 15:49:26 -0300

    feat(aws): update aws configs
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- modules/cloud/common.nix
+++ modules/cloud/common.nix
@@ -159,8 +159,10 @@ k9s:
       shellAliases = {
        k9s_prod = "k9s --kubeconfig ~/.dotfiles.nix/zapay/kube/zapay-production.yaml"; 
        k9s_stg = "k9s --kubeconfig ~/.dotfiles.nix/zapay/kube/zapay-staging.yaml";
-       aws_login_prod = "aws sso login --profile Fabio_Souza-071032557399";
-       aws_login_stg = "aws sso login --profile Fabio_Souza 901943060028";
+       aws_zapay_login = "aws sso login --sso-session aws-zapay" ;
+       aws_zapay_kube_prod = "aws eks update-kubeconfig --region sa-east-1 --name zapay-one --profile aws-zapay-prod";
+       aws_zapay_kube_stg = "aws eks update-kubeconfig --region us-east-2 --name zpy-k8s-cluster-staging --profile aws-zapay-stg";
+       aws_zapay_baixar_imagem_api = "aws ecr get-login-password --region sa-east-1 --profile aws-zapay-stg | docker login --username AWS --password-stdin 071032557399.dkr.ecr.sa-east-1.amazonaws.com";
       };
     };
 
