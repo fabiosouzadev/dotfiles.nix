a315fbb842c34bab9feb17152b7d192e4f7a8d04
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-04-24 15:42:22 -0300

    feat(gh): config gh function
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- home/tui/git.nix
+++ home/tui/git.nix
@@ -87,5 +87,15 @@ in {
     shellAliases = {
       gitgrep = "git ls-files | rg";
     };
+    initExtra = ''
+      ghpr() {
+        GH_FORCE_TTY="100%" gh pr list | \
+        fzf --ansi --preview \
+          'GH_FORCE_TTY="100%" gh pr view {1}' \
+          --preview-window down  --header-lines 3 | awk '{print $1}' \
+        | xargs gh pr checkout
+
+      }
+    '';
   };
 }
