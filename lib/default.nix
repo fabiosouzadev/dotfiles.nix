b793576ed331ef8ced499c52ee6d5931449dd89c
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-04-29 16:01:39 -0300

    chore(format): nix files formatted
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- lib/default.nix
+++ lib/default.nix
@@ -1,32 +1,35 @@
-{ inputs }:
-let
-defaultUsername = "fabiosouzadev";
-homeManagerShared = import ./shared/home-manager.nix;
-homeManagerNixos  = import ./nixos/home-manager.nix;
-#homeManagerDarwin = import ./darwin/home-manager.nix;
+{inputs}: let
+  defaultUsername = "fabiosouzadev";
+  homeManagerShared = import ./shared/home-manager.nix;
+  homeManagerNixos = import ./nixos/home-manager.nix;
+  #homeManagerDarwin = import ./darwin/home-manager.nix;
 in {
   #mkDarwim = {};
- 
-  mkNixos = {
-    username ? defaultUsername,
-  }: {hostname, system, isDesktop, hasVirtualisation}:
-   inputs.nixpkgs.lib.nixosSystem {
-     inherit system;
-     specialArgs = { inherit username isDesktop hasVirtualisation; };
-     modules = [
+
+  mkNixos = {username ? defaultUsername}: {
+    hostname,
+    system,
+    isDesktop,
+    hasVirtualisation,
+  }:
+    inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {inherit username isDesktop hasVirtualisation;};
+      modules = [
         ../configuration
         ../hosts/${hostname}
-        inputs.home-manager.nixosModules.home-manager {
+        inputs.home-manager.nixosModules.home-manager
+        {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit inputs username isDesktop; };
+          home-manager.extraSpecialArgs = {inherit inputs username isDesktop;};
           home-manager.users."${username}" = {
             imports = [
               homeManagerShared
               homeManagerNixos
             ];
           };
-       }
-     ];
-   };
+        }
+      ];
+    };
 }
