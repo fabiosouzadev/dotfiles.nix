da8e7802baa04c9a10545ca9e1d4ca6d4fee2dce
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-04-29 16:03:39 -0300

    chore(flake): replace formatter
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- flake.nix
+++ flake.nix
@@ -5,41 +5,61 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     fabiosouzadev-nvim.url = "github:fabiosouzadev/neovim-nix";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-  let
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }: let
     mkDarwin = self.lib.mkDarwin {};
     mkNixos = self.lib.mkNixos {};
   in
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    flake-parts.lib.mkFlake {inherit inputs;} {
       flake = {
-
         lib = import ./lib {inherit inputs;};
 
         nixosConfigurations = {
-          nix-zapay = mkNixos {hostname = "nix-zapay"; system = "x86_64-linux"; isDesktop = true; hasVirtualisation = true;};
-          vm = mkNixos {hostname = "vm"; system = "x86_64-linux"; isDesktop = true; hasVirtualisation = false;};
+          nix-zapay = mkNixos {
+            hostname = "nix-zapay";
+            system = "x86_64-linux";
+            isDesktop = true;
+            hasVirtualisation = true;
+          };
+          vm = mkNixos {
+            hostname = "vm";
+            system = "x86_64-linux";
+            isDesktop = true;
+            hasVirtualisation = false;
+          };
         };
 
         #darwinConfigurations = {};
       };
       systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
-      perSystem = { config, self', inputs',pkgs,system, ... }: {
-      	devShells = {
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells = {
           default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [ just ];
+            nativeBuildInputs = with pkgs; [just];
           };
         };
-	      formatter = pkgs.alejandra;
-      }; 
+        formatter = pkgs.nixpkgs-fmt;
+      };
 
-    #packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    
-    #packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-  };
+      #packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+      #packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    };
 }
