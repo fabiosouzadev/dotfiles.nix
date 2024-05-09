28caf11f75019bcf1c680dd8747ec35834f023ae
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-05-09 16:21:13 -0300

    feat(aws,zapay): add vpn config

--- home/cloud/aws.nix
+++ home/cloud/aws.nix
@@ -11,6 +11,7 @@
       aws_zapay_kube_prod = "aws eks update-kubeconfig --region sa-east-1 --name zapay-one --profile aws-zapay-prod";
       aws_zapay_kube_stg = "aws eks update-kubeconfig --region us-east-2 --name zpy-k8s-cluster-staging --profile aws-zapay-stg";
       aws_zapay_baixar_imagem_api = "aws ecr get-login-password --region sa-east-1 --profile aws-zapay-prod | docker login --username AWS --password-stdin 071032557399.dkr.ecr.sa-east-1.amazonaws.com";
+      aws_vpn = "awsvpnclient start --config ~/.dotfiles.nix/zapay/aws/vpn/config.ovpn";
     };
   };
 }
