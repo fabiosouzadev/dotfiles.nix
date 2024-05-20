3d4eeb501213fccc5abf85318aefbc02a0597584
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-05-20 16:44:35 -0300

    chore(nix): chore nix configurations

--- configuration/nix.nix
+++ configuration/nix.nix
@@ -1,28 +1,28 @@
 { pkgs, username, ... }:
 {
-  # nix = {
-  #   settings = {
-  #     auto-optimise-store = true;
-  #     builders-use-substitutes = true;
-  #     experimental-features = [ "nix-command" "flakes" ];
-  #     substituters = [
-  #       "https://cache.nixos.org"
-  #       "https://nix-community.cachix.org"
-  #     ];
-  #     trusted-public-keys = [
-  #       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-  #       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  #     ];
-  #     trusted-users = [ username ];
-  #     warn-dirty = false;
-  #   };
-  #   # do garbage collection weekly to keep disk usage low
-  #   gc = {
-  #     automatic = true;
-  #     dates = "weekly";
-  #     options = "--delete-older-than 3d";
-  #   };
-  # };
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      builders-use-substitutes = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      trusted-users = [ username ];
+      warn-dirty = false;
+    };
+    # do garbage collection weekly to keep disk usage low
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 3d";
+    };
+  };
   nixpkgs = {
     config = {
       allowUnfree = true;
