a36476625e5faeab64c9bf77c73750e5c0ea7a3f
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-05-21 18:03:33 -0300

    fix(gpg): fix config-keys
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- home/tui/gpg.nix
+++ home/tui/gpg.nix
@@ -8,6 +8,7 @@
     maxCacheTtl = 31536000;
     maxCacheTtlSsh = 31536000;
     enableSshSupport = true;
-    pinentryPackage = pkgs.pinentry-tty;
+    # pinentryPackage = pkgs.pinentry-tty;
+    pinentryPackage = pkgs.pinentry-curses;
   };
 }
