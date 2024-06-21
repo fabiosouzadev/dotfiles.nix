64c1992caf7465ff5f46a9b154d3308ff4e8ba31
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-06-21 09:38:54 -0300

    feat: update packages

--- modules/jetbrains.nix
+++ modules/jetbrains.nix
@@ -1,13 +1,13 @@
-{  pkgs, ... }:
+{ pkgs, ... }:
 
 {
-   environment.systemPackages = with pkgs; [
-    
+  environment.systemPackages = with pkgs; [
+
     #Pycharm
-    jetbrains.pycharm-community-src # from source
+    #jetbrains.pycharm-community-src # from source
     #jetbrains.pycharm-community-bin # patched binaries from jetbrains
     #jetbrains.pycharm-community # patched binaries from jetbrains
-    
+
     # Idea
     #jetbrains.idea-community-src
     #jetbrains.idea-community-bin
