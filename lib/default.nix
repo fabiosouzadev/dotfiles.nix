720642f22394099f68295508d07505cac00ad1db
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-06-21 09:49:13 -0300

    style: format file

--- lib/default.nix
+++ lib/default.nix
@@ -1,20 +1,21 @@
-{inputs}: let
+{ inputs }:
+let
   defaultUsername = "fabiosouzadev";
   homeManagerShared = import ./shared/home-manager.nix;
   homeManagerNixos = import ./nixos/home-manager.nix;
   #homeManagerDarwin = import ./darwin/home-manager.nix;
-in {
+in
+{
   #mkDarwim = {};
-
-  mkNixos = {username ? defaultUsername}: {
-    hostname,
-    system,
-    isDesktop,
-    hasVirtualisation,
-  }:
+  mkNixos = { username ? defaultUsername }: { hostname
+                                            , system
+                                            , isDesktop
+                                            , hasVirtualisation
+                                            ,
+                                            }:
     inputs.nixpkgs.lib.nixosSystem {
       inherit system;
-      specialArgs = {inherit inputs username isDesktop hasVirtualisation;};
+      specialArgs = { inherit inputs username isDesktop hasVirtualisation; };
       modules = [
         ../configuration
         ../hosts/${hostname}
@@ -22,7 +23,7 @@ in {
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = {inherit inputs username isDesktop;};
+          home-manager.extraSpecialArgs = { inherit inputs username isDesktop; };
           home-manager.users."${username}" = {
             imports = [
               homeManagerShared
