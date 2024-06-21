b43b4e126d1a91454fefd7f7f692ac564b0badd9
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-06-21 15:47:49 -0300

    feat: add Tabnine and Cody

--- home/tui/zsh.nix
+++ home/tui/zsh.nix
@@ -1,4 +1,4 @@
-{...}: {
+{ ... }: {
   # basic configuration of git, please change to your own
   programs.zsh = {
     enable = true;
@@ -10,9 +10,11 @@
       size = 10000000;
       share = true;
     };
-    # sessionVariables = {
-    #   GITHUB_TOKEN = "pass Github/access_token_gh";
-    # };
+    sessionVariables = {
+      # GITHUB_TOKEN = "pass Github/access_token_gh";
+      SRC_ACCESS_TOKEN = "pass Sourcegraph/access_token_sg";
+      SRC_ENDPOINT = "pass Sourcegraph/endpoint_sg";
+    };
     # shellAliases = {
     # };
   };
