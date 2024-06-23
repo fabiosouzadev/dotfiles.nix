8bce404fbd2e769e18e128021dc61074355608e7
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-06-23 02:08:43 -0300

    chore: add initExtra
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- home/tui/zsh.nix
+++ home/tui/zsh.nix
@@ -10,11 +10,18 @@
       size = 10000000;
       share = true;
     };
-    sessionVariables = {
-      # GITHUB_TOKEN = "pass Github/access_token_gh";
-      SRC_ACCESS_TOKEN = "pass Sourcegraph/access_token_sg";
-      SRC_ENDPOINT = "pass Sourcegraph/endpoint_sg";
-    };
+    initExtra = ''
+      if [[ -o interactive ]]; then
+          SRC_ACCESS_TOKEN = $(pass Sourcegraph/access_token_sg)
+          SRC_ENDPOINT = $(pass Sourcegraph/endpoint_sg)
+          OPENAI_API_KEY = $(pass OpenAI/api_key_openai)
+      fi
+    '';
+    # sessionVariables = {
+    #   # GITHUB_TOKEN = "pass Github/access_token_gh";
+    #   #SRC_ACCESS_TOKEN = "";
+    #   #SRC_ENDPOINT = "";
+    # };
     # shellAliases = {
     # };
   };
