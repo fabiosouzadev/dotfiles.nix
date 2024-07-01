a7795fd3f8a39796d483291f77474cceed8fde9b
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-07-01 20:29:03 -0300

    chore: add xfce as desktop manager
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- configuration/default.nix
+++ configuration/default.nix
@@ -1,16 +1,22 @@
-{ lib, hasVirtualisation, ... }:
 {
-  imports = [
-    ./user.nix
-    ./nix.nix
-    ./system-packages.nix
-    ./localization.nix
-    ./keymap.nix
-    ../modules/fonts.nix
-    ../modules/i3.nix
-    ../modules/jetbrains.nix
-    ../modules/polkit.nix
-    ../modules/vscodium.nix
-    # ../modules/neovim.nix
-  ] ++ lib.optionals hasVirtualisation [ ./virtualisation ];
+  lib,
+  hasVirtualisation,
+  ...
+}: {
+  imports =
+    [
+      ./user.nix
+      ./nix.nix
+      ./system-packages.nix
+      ./localization.nix
+      ./keymap.nix
+      ../modules/fonts.nix
+      # ../modules/i3.nix
+      ../modules/xfce_i3.nix
+      ../modules/jetbrains.nix
+      ../modules/polkit.nix
+      ../modules/vscodium.nix
+      # ../modules/neovim.nix
+    ]
+    ++ lib.optionals hasVirtualisation [./virtualisation];
 }
