e66e23311e46fa00e82a2e635af0906aea90234a
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-09-17 09:34:31 -0300

    chore: remove sync and remove some aws configs

--- configuration/secrets/zapay.nix
+++ configuration/secrets/zapay.nix
@@ -101,81 +101,81 @@ in {
     #   path = "${homepath}/.aws/config";
     #   owner = config.users.users.${username}.name;
     # };
-    templates."zapay_aws_staging" = {
-      content = ''
-        apiVersion: v1
-        clusters:
-        - cluster:
-            certificate-authority-data: ${config.sops.placeholder."aws-zapay/staging/cluster_certificate_staging"}
-            server: ${config.sops.placeholder."aws-zapay/staging/cluster_server_staging"}
-          name: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
-        contexts:
-        - context:
-            cluster: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
-            user: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
-          name: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
-        current-context: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
-        kind: Config
-        preferences: {}
-        users:
-        - name: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
-          user:
-            exec:
-              apiVersion: client.authentication.k8s.io/v1beta1
-              args:
-              - --region
-              - us-east-2
-              - eks
-              - get-token
-              - --cluster-name
-              - zpy-k8s-cluster-staging
-              - --output
-              - json
-              command: aws
-              env:
-              - name: AWS_PROFILE
-                value: aws-zapay-stg
-      '';
-      path = "${homepath}/.kube/zapay_aws_staging.yaml";
-      owner = config.users.users.${username}.name;
-    };
-    templates."zapay_aws_production" = {
-      content = ''
-        apiVersion: v1
-        clusters:
-        - cluster:
-            certificate-authority-data: ${config.sops.placeholder."aws-zapay/production/cluster_certificate_production"}
-            server: ${config.sops.placeholder."aws-zapay/production/cluster_server_production"}
-          name: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
-        contexts:
-        - context:
-            cluster: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
-            user: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
-          name: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
-        current-context: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
-        kind: Config
-        preferences: {}
-        users:
-        - name: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
-          user:
-            exec:
-              apiVersion: client.authentication.k8s.io/v1beta1
-              args:
-              - --region
-              - sa-east-1
-              - eks
-              - get-token
-              - --cluster-name
-              - zapay-one
-              - --output
-              - json
-              command: aws
-              env:
-              - name: AWS_PROFILE
-                value: aws-zapay-prod
-      '';
-      path = "${homepath}/.kube/zapay_aws_production.yaml";
-      owner = config.users.users.${username}.name;
-    };
+    # templates."zapay_aws_staging" = {
+    #   content = ''
+    #     apiVersion: v1
+    #     clusters:
+    #     - cluster:
+    #         certificate-authority-data: ${config.sops.placeholder."aws-zapay/staging/cluster_certificate_staging"}
+    #         server: ${config.sops.placeholder."aws-zapay/staging/cluster_server_staging"}
+    #       name: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
+    #     contexts:
+    #     - context:
+    #         cluster: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
+    #         user: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
+    #       name: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
+    #     current-context: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
+    #     kind: Config
+    #     preferences: {}
+    #     users:
+    #     - name: ${config.sops.placeholder."aws-zapay/staging/cluster_name_staging"}
+    #       user:
+    #         exec:
+    #           apiVersion: client.authentication.k8s.io/v1beta1
+    #           args:
+    #           - --region
+    #           - us-east-2
+    #           - eks
+    #           - get-token
+    #           - --cluster-name
+    #           - zpy-k8s-cluster-staging
+    #           - --output
+    #           - json
+    #           command: aws
+    #           env:
+    #           - name: AWS_PROFILE
+    #             value: aws-zapay-stg
+    #   '';
+    #   path = "${homepath}/.kube/zapay_aws_staging.yaml";
+    #   owner = config.users.users.${username}.name;
+    # };
+    # templates."zapay_aws_production" = {
+    #   content = ''
+    #     apiVersion: v1
+    #     clusters:
+    #     - cluster:
+    #         certificate-authority-data: ${config.sops.placeholder."aws-zapay/production/cluster_certificate_production"}
+    #         server: ${config.sops.placeholder."aws-zapay/production/cluster_server_production"}
+    #       name: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
+    #     contexts:
+    #     - context:
+    #         cluster: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
+    #         user: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
+    #       name: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
+    #     current-context: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
+    #     kind: Config
+    #     preferences: {}
+    #     users:
+    #     - name: ${config.sops.placeholder."aws-zapay/production/cluster_name_production"}
+    #       user:
+    #         exec:
+    #           apiVersion: client.authentication.k8s.io/v1beta1
+    #           args:
+    #           - --region
+    #           - sa-east-1
+    #           - eks
+    #           - get-token
+    #           - --cluster-name
+    #           - zapay-one
+    #           - --output
+    #           - json
+    #           command: aws
+    #           env:
+    #           - name: AWS_PROFILE
+    #             value: aws-zapay-prod
+    #   '';
+    #   path = "${homepath}/.kube/zapay_aws_production.yaml";
+    #   owner = config.users.users.${username}.name;
+    # };
   };
 }
