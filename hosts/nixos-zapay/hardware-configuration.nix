b997a0d1900088ec2fc8a52634d427ebc121791d
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-11-10 22:34:00 -0300

    chore: hardware configs

--- hosts/nixos-zapay/hardware-configuration.nix
+++ hosts/nixos-zapay/hardware-configuration.nix
@@ -35,7 +35,7 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.br-cbe5184ea129.useDHCP = lib.mkDefault true;
   # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.veth53ca268.useDHCP = lib.mkDefault true;
+  # networking.interfaces.veth9a3d75c.useDHCP = lib.mkDefault true;
   # networking.interfaces.virbr0.useDHCP = lib.mkDefault true;
   # networking.interfaces.vnet0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
