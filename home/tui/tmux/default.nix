5fd6c95412c3642ffdb7dd51818e37280d860ebd
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-12-14 14:49:34 -0300

    chore: update nvim and add tmux configs

--- home/tui/tmux/default.nix
+++ home/tui/tmux/default.nix
@@ -35,6 +35,8 @@
           set -g @resurrect-dir $resurrect_dir
           set -g @resurrect-hook-post-save-all "sed -i 's/--cmd lua.*--cmd set packpath/--cmd \"lua/g; s/--cmd set rtp.*\$/\"/' $resurrect_dir/last"
           set -g @resurrect-capture-pane-contents 'on'
+          # for neovim
+          set -g @resurrect-strategy-nvim 'session'
         '';
       }
       {
