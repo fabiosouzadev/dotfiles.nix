ed787d92e97fca05e472e5a2800e67b1bd667c9e
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-12-17 03:11:30 -0300

    chore: update gpg configs

--- home/tui/gpg.nix
+++ home/tui/gpg.nix
@@ -2,12 +2,15 @@
   programs.gpg.enable = true;
   services.gpg-agent = {
     enable = true;
+    enableZshIntegration = true;
     defaultCacheTtl = 34560000; # cache keys forever don't get asked for password
     defaultCacheTtlSsh = 34560000; # cache keys forever don't get asked for password
     maxCacheTtl = 34560000;
     maxCacheTtlSsh = 34560000;
     enableSshSupport = true;
-    pinentryPackage = pkgs.pinentry-tty;
+    # pinentryPackage = pkgs.pinentry-tty;
     # pinentryPackage = pkgs.pinentry-curses;
+    pinentryPackage = pkgs.pinentry-gnome3;
+    sshKeys = ["FBB635DEEB7F34838A309D34366402035F3025A8"];
   };
 }
