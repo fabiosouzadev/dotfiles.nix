d0fa363f23ad82eb6994c97aa563a4c4f978e76f
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-12-17 12:44:23 -0300

    chore: add gpg more configs

--- home/tui/gpg.nix
+++ home/tui/gpg.nix
@@ -8,8 +8,8 @@
     maxCacheTtl = 34560000;
     maxCacheTtlSsh = 34560000;
     enableSshSupport = true;
-    pinentryPackage = pkgs.pinentry-tty;
-    # pinentryPackage = pkgs.pinentry-curses;
+    # pinentryPackage = pkgs.pinentry-tty;
+    pinentryPackage = pkgs.pinentry-curses;
     # pinentryPackage = pkgs.pinentry-gtk2;
     sshKeys = ["FBB635DEEB7F34838A309D34366402035F3025A8"];
     enableScDaemon = false;
