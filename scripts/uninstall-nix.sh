87367c9586756b60da715ad34b27334c75313890
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-01-06 17:28:28 -0300

    fix: scripts

--- scripts/uninstall-nix.sh
+++ scripts/uninstall-nix.sh
@@ -6,6 +6,8 @@ sudo systemctl daemon-reload
 
 sudo rm -rf /etc/nix /etc/profile.d/nix.sh /etc/tmpfiles.d/nix-daemon.conf /nix ~root/.nix-channels ~root/.nix-defexpr ~root/.nix-profile
 
+sudo rm -rf /home/fabiosouzadev/.local/state/nix
+sudo rm -rf /home/fabiosouzadev/.local/state/home-manager/
 
 for i in $(seq 1 32); do
   sudo userdel nixbld$i
