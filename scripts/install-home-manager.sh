a0241bb85ff4579810be6cb1b8ed533d296f3154
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-01-06 12:15:47 -0300

    feat: add install home-manager

--- scripts/install-home-manager.sh
+++ scripts/install-home-manager.sh
@@ -1,5 +1,7 @@
 
 
 #echo "experimental-features = nix-command flakes" > /etc/nix/nix.conf
-echo 'experimental-features = nix-command flakes' | sudo tee /etc/nix/nix.conf
+#echo 'experimental-features = nix-command flakes' | sudo tee /etc/nix/nix.conf
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
 nix-shell '<home-manager>' -A install 
