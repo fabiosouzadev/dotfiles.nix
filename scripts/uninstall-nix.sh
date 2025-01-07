fc762d99610db4a742b0313b39e443edf892a5d1
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-01-07 18:31:33 -0300

    fix: home-manager install

--- scripts/uninstall-nix.sh
+++ scripts/uninstall-nix.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+home-manager uninstall
 sudo systemctl stop nix-daemon.service
 sudo systemctl disable nix-daemon.socket nix-daemon.service
 sudo systemctl daemon-reload
@@ -7,7 +9,7 @@ sudo systemctl daemon-reload
 sudo rm -rf /etc/nix /etc/profile.d/nix.sh /etc/tmpfiles.d/nix-daemon.conf /nix ~root/.nix-channels ~root/.nix-defexpr ~root/.nix-profile
 
 sudo rm -rf /home/fabiosouzadev/.local/state/nix
-sudo rm -rf /home/fabiosouzadev/.local/state/home-manager
+sudo rm -rf /home/fabiosouzadev/.local/state/home-manager/
 
 for i in $(seq 1 32); do
   sudo userdel nixbld$i
