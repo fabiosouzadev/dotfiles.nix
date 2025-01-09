4b9110aac0e383dd27a9a50e002a2437d9ee18e6
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-01-09 01:37:11 -0300

    fix: neovim

--- modules/home-manager/editors/neovim.nix
+++ modules/home-manager/editors/neovim.nix
@@ -3,9 +3,11 @@
   neovim-flake,
   ...
 }: {
+  home.packages = [
+    neovim-flake.packages.${pkgs.system}.default
+  ];
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    package = neovim-flake.packages.${pkgs.system}.default;
   };
 }
