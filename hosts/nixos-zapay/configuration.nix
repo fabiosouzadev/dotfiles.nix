0fa7341e9b184198305dcc91cac69d5549f69441
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-01-10 03:25:37 -0300

    fix: fix more configs

--- hosts/nixos-zapay/configuration.nix
+++ hosts/nixos-zapay/configuration.nix
@@ -1,16 +1,14 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -39,9 +37,8 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
+
+  
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
@@ -51,7 +48,7 @@
   # services.printing.enable = true;
 
   # Enable sound.
-  # hardware.pulseaudio.enable = true;
+  # services.pulseaudio.enable = true;
   # OR
   # services.pipewire = {
   #   enable = true;
@@ -120,5 +117,7 @@
   # and migrated your data accordingly.
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
+
 }
+
