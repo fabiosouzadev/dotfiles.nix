1aef03d56e1c18064096c8e1e5f4281b17f8f339
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-01-16 17:00:34 -0300

    chore: more adjusts

--- hosts/dell-inspirion-3520/configuration.nix
+++ hosts/dell-inspirion-3520/configuration.nix
@@ -56,7 +56,7 @@
   # };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
