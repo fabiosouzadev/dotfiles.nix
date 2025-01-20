853659dc59975ae1a5285ed5f1f6909b832ad074
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-01-20 17:58:06 -0300

    chore: add ghostty configs

--- modules/home/terminal/emulators/ghostty/default.nix
+++ modules/home/terminal/emulators/ghostty/default.nix
@@ -1,7 +1,14 @@
-{...}: {
+{
   programs.ghostty = {
     enable = true;
     enableZshIntegration = true;
     enableBashIntegration = true;
+    enableFishIntegration = true;
+    settings = {
+      theme = "rose-pine-moon";
+      title = " ";
+      window-decoration = "false";
+      gtk-adwaita = "false";
+    };
   };
 }
