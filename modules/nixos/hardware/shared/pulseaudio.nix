e43c280534dafa644c8a691841b088e70a55449f
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-01-29 15:56:37 -0300

    fix: fix switch warnings

--- modules/nixos/hardware/shared/pulseaudio.nix
+++ modules/nixos/hardware/shared/pulseaudio.nix
@@ -1,5 +1,25 @@
-{lib,pkgs, ...}: {
-  hardware.pulseaudio = {
+{
+  lib,
+  pkgs,
+  vars,
+  ...
+}: {
+  users.extraUsers.${vars.username}.extraGroups = ["audio"];
+  # hardware.pulseaudio = {
+  #   enable = true;
+  #   support32Bit = true;
+  #   package = pkgs.pulseaudioFull.override {jackaudioSupport = true;};
+  #   extraConfig = "
+  #         load-module module-switch-on-connect
+  #         load-module module-combine-sink # if you want a combined channel where all output devices are part of
+  #         load-module module-bluetooth-policy
+  #         # load-module module-bluetooth-discover
+  #         load-module module-bluez5-device
+  #         load-module module-bluez5-discover
+  #    ";
+  # };
+
+  services.pulseaudio = {
     enable = true;
     support32Bit = true;
     package = pkgs.pulseaudioFull.override {jackaudioSupport = true;};
