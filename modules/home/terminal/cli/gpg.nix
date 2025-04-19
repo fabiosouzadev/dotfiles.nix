db78acca627b3361e567b6be6d79f9e40e591e9f
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-04-19 15:50:22 -0300

    fix: fix ssh in nixos

--- modules/home/terminal/cli/gpg.nix
+++ modules/home/terminal/cli/gpg.nix
@@ -7,7 +7,7 @@
     defaultCacheTtlSsh = 34560000; # cache keys forever don't get asked for password
     maxCacheTtl = 34560000;
     maxCacheTtlSsh = 34560000;
-    enableSshSupport = true;
+    # enableSshSupport = true;
     pinentryPackage = pkgs.pinentry-tty;
     # pinentryPackage = pkgs.pinentry-curses;
     #pinentryPackage = pkgs.pinentry-gtk2;
