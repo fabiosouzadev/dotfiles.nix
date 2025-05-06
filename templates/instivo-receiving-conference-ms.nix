b1c986b614ce1320d1dae800a04252709357d6cc
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-05-06 17:49:25 -0300

    doc(README.md): change instivo ms-conference flake template

--- templates/instivo-receiving-conference-ms.nix
+++ templates/instivo-receiving-conference-ms.nix
@@ -5,33 +5,34 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    devShells.x86_64-linux.nodejs = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        openssl
-        nodejs
-        nodePackages.npm
-        prisma
-      ];
-      shellHook = ''
-        mkdir -p .nix-node
-        export NODE_PATH=$PWD/.nix-node
-        export PATH=$NODE_PATH/bin:$PATH
-        npm config set prefix $NODE_PATH
-         export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
-         export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
-         export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
-         export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
-      '';
-    };
-  };
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          openssl
+          nodePackages.npm
+          nodePackages.prisma
+        ];
+        shellHook = with pkgs; ''
+          export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+          export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+          export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+          export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+          export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+          npm install
+          prisma generate
+          # prisma migrate dev
+        '';
+      };
+    });
 }
