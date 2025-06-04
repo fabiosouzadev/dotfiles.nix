9ffce09238341d36b387acf6bc715829f811ccf1
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-06-04 12:18:46 -0300

    chore(nvim,configs): nvim updated,updated outhers configs

--- modules/home/terminal/cli/git.nix
+++ modules/home/terminal/cli/git.nix
@@ -53,7 +53,7 @@ in {
       gitgrep = "git ls-files | rg";
       gdd = "git add";
       gcm = "git commit -m";
-      gco = "git commit -a -m";
+      gco = "git add . && git commit -m";
       gcl = "git clone";
       gps = "git push";
       gpl = "git pull";
--- modules/nixos/services/ollama-optimized.nix
+++ modules/nixos/services/ollama-optimized.nix
@@ -53,19 +53,19 @@
   '';
 
   ## Open-webui http://127.0.0.1:11435
-  services.open-webui = {
-    enable = true;
-    port = 11435;
-    environment = {
-      ANONYMIZED_TELEMETRY = "False";
-      DO_NOT_TRACK = "True";
-      SCARF_NO_ANALYTICS = "True";
-      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
-      OLLAMA_BASE_URL = "http://127.0.0.1:11434";
-      # # Disable authentication
-      WEBUI_AUTH = "False";
-    };
-  };
+  # services.open-webui = {
+  #   enable = true;
+  #   port = 11435;
+  #   environment = {
+  #     ANONYMIZED_TELEMETRY = "False";
+  #     DO_NOT_TRACK = "True";
+  #     SCARF_NO_ANALYTICS = "True";
+  #     OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+  #     OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+  #     # # Disable authentication
+  #     WEBUI_AUTH = "False";
+  #   };
+  # };
 
   # Ai for cli https://github.com/block/goose
   # Ai for cli https://github.com/ggozad/oterm
