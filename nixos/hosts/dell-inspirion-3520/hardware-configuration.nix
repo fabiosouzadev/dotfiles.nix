53728f5d61f9ea34839f55d838fa360d90844026
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-07-29 12:18:13 -0300

    chore(flake.lock): update flake.lock

--- nixos/hosts/dell-inspirion-3520/hardware-configuration.nix
+++ nixos/hosts/dell-inspirion-3520/hardware-configuration.nix
@@ -14,30 +14,18 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/d078af59-46bc-47fc-85dc-0855897e73c6";
+    { device = "/dev/disk/by-uuid/e0110ece-fdd4-47fb-8e3b-6e1cfe597216";
       fsType = "btrfs";
       options = [ "subvol=@" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/4D5D-B104";
+    { device = "/dev/disk/by-uuid/745A-A146";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/7d3ba6b3-c87e-4649-90aa-dceba87fdd53"; }
-    ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.virbr0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.vnet0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
+  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
